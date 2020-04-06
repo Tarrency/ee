6aa3ba99565d5515007e87804fdfdea78eb20c84
CAD <cusucad0406>
2020-04-06 18:11:13 +0800

    dockerfile

--- src/main/java/com/cusc/cuscai/controller/DemoController.java
+++ src/main/java/com/cusc/cuscai/controller/DemoController.java
@@ -1,4 +1,12 @@
 package com.cusc.cuscai.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello cuscai";
+    }
 }
