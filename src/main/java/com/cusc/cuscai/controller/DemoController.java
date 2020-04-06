48175db23b31264bdfeb1329ef68095fdf3f948b
CAD <cusucad0406>
2020-04-06 18:26:29 +0800

    自动部署测试

--- src/main/java/com/cusc/cuscai/controller/DemoController.java
+++ src/main/java/com/cusc/cuscai/controller/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("/")
     public String sayHello() {
-        return "hello cuscai";
+        return "hello from cuscai";
     }
 }
