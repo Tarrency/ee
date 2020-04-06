5781aa3d78f7eabdd1bdaec2672f0e3a7f06ab53
CAD <cusucad0406>
2020-04-06 23:02:35 +0800

    跨域处理

--- src/main/java/com/cusc/cuscai/config/WebMvcConfig.java
+++ src/main/java/com/cusc/cuscai/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.cusc.cuscai.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    /**
+     * 配置静态资源
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
@@ -20,4 +24,22 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
+    /**
+     * 跨域处理
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //添加映射路径
+        registry.addMapping("/**")
+                //放行哪些原始域
+                .allowedOrigins("*")
+                //是否发送Cookie信息
+                .allowCredentials(true)
+                //放行哪些原始域(请求方式)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION")
+                //放行哪些原始域(头部信息)
+                .allowedHeaders("*")
+                //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+                .exposedHeaders("Header1", "Header2");
+    }
 }
--- src/main/java/com/cusc/cuscai/util/Result.java
+++ src/main/java/com/cusc/cuscai/util/Result.java
@@ -1,17 +1,23 @@
 package com.cusc.cuscai.util;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
  * 统一返回
  */
 @Data
+@ApiModel("返回体")
 public class Result {
     //状态码
+    @ApiModelProperty(value = "状态码")
     private Integer code;
     //描述信息
+    @ApiModelProperty(value = "描述信息")
     private String msg;
     //返回数据
+    @ApiModelProperty(value = "返回数据")
     private Object data;
 
     private Result(Integer code, String msg, Object data) {
