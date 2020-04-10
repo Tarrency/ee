015c509b9f0fc9cddab1cd8420c83b34823c291b
lipssmycode <544450236@qq.com>
2020-04-10 10:00:50 +0800

    test

--- src/main/java/com/cusc/cuscai/controller/DemoController.java
+++ src/main/java/com/cusc/cuscai/controller/DemoController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("/")
     public Result sayHello() {
-        return Result.success("hello from cuscai!!!!!");
+        return Result.success("hello from cuscai!");
     }
 }
