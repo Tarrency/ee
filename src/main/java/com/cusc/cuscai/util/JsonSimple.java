b2168d5fb4b099794c904021ef5bbe56044a29d4
lipssmycode <544450236@qq.com>
2020-04-24 17:08:09 +0800

    修改StringUtils引入的包

--- src/main/java/com/cusc/cuscai/util/JsonSimple.java
+++ src/main/java/com/cusc/cuscai/util/JsonSimple.java
@@ -1,14 +1,13 @@
 package com.cusc.cuscai.util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.util.Collection;
