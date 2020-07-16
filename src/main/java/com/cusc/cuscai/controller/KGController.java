457f0ee269bfa907fcb38fc458e77d709e16ee93
liangjingru <guavaljr@foxmail.com>
2020-07-16 10:18:07 +0800

    修改了kgcontroller接口的返回码

--- src/main/java/com/cusc/cuscai/controller/KGController.java
+++ src/main/java/com/cusc/cuscai/controller/KGController.java
@@ -21,151 +21,226 @@ public class KGController {
     @Autowired
     private KGServer kgServer;
 
-    @GetMapping("hello")
-    public String hello() {
-        return "hello";
-    }
-
     @ApiOperation("获取实体标签")
     @GetMapping("getEntLabels")
     public Result getEntLabels() {
-        return Result.success("查询实体标签", kgServer.getEntLabels());
+        List<String> res;
+        try {
+            res = kgServer.getEntLabels();
+        } catch (Exception e) {
+            return Result.fail(40400, e.getMessage());
+        }
+        return Result.success(20400,"查询实体标签", res);
     }
 
-    @ApiOperation("获取关系标签")
-    @GetMapping("getRelLabels")
-    public Result getRelLabels() {
-        return Result.success("查询关系标签", kgServer.getRelLabels());
-    }
+//    @ApiOperation("获取关系标签")
+//    @GetMapping("getRelLabels")
+//    public Result getRelLabels() {
+//        return Result.success("查询关系标签", kgServer.getRelLabels());
+//    }
+//
+//    @ApiOperation("统计各类实体数量")
+//    @GetMapping("countEntities")
+//    public Result countEntities() {
+//        return Result.success("统计各类实体数量", kgServer.countEntities());
+//    }
+//
+//    @ApiOperation("统计关系数量")
+//    @GetMapping("countRelations")
+//    public Result countRelations() {
+//        return Result.success("统计关系数量", kgServer.countRelations());
+//    }
 
-    @ApiOperation("统计各类实体数量")
-    @GetMapping("countEntities")
-    public Result countEntities() {
-        return Result.success("统计各类实体数量", kgServer.countEntities());
-    }
-
-    @ApiOperation("统计关系数量")
-    @GetMapping("countRelations")
-    public Result countRelations() {
-        return Result.success("统计关系数量", kgServer.countRelations());
-    }
-
-    // 正确的前后端逻辑应该是 前端选择一个实体类型，后端返回给实体属性，然后前端再输入属性具体值发给后端进行添加
+    /**
+     * @param propertyList 属性列表，格式为属性名:属性值
+     * @return
+     */
     @ApiOperation("添加实体")
     @PostMapping("addNode")
     public Result addNode(@RequestBody @ApiParam(value = "json字符串", required = true) JSONObject propertyList) {
-        Object node = kgServer.addNode(propertyList);
-        return Result.success("添加实体成功", node);
+        Object node;
+        try{
+            node = kgServer.addNode(propertyList);
+        } catch (Exception e) {
+            return Result.fail(40401, e.getMessage());
+        }
+        return Result.success(20401,"添加实体成功", node);
     }
 
+    /**
+     *
+     * @param name
+     * @return
+     */
     @ApiOperation("查找节点")
     @PostMapping("/findNode")
     public Result findNodeByName(@RequestParam("name") @ApiParam(value = "name", required = true) String name) {
         System.out.println(name);
-        List<Object> nodes = kgServer.findNodeByName(name);
+        List<Object> nodes;
         List<Map<String, Object>> res = new ArrayList<>();
-        for (int i = 0; i < nodes.size(); i++) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("id", ((NodeModel) nodes.get(i)).getId());
-            map.put("type", ((NodeModel) nodes.get(i)).getLabels()[0]);
-            for (int j = 0; j < ((NodeModel) nodes.get(i)).getPropertyList().size(); j++) {
-                if (((NodeModel) nodes.get(i)).getPropertyList().get(j).getKey().equals("name")) {
-                    map.put("name", ((NodeModel) nodes.get(i)).getPropertyList().get(j).getValue());
+        try{
+            nodes = kgServer.findNodeByName(name);
+            for (int i = 0; i < nodes.size(); i++) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", ((NodeModel) nodes.get(i)).getId());
+                map.put("type", ((NodeModel) nodes.get(i)).getLabels()[0]);
+                for (int j = 0; j < ((NodeModel) nodes.get(i)).getPropertyList().size(); j++) {
+                    if (((NodeModel) nodes.get(i)).getPropertyList().get(j).getKey().equals("name")) {
+                        map.put("name", ((NodeModel) nodes.get(i)).getPropertyList().get(j).getValue());
+                    }
                 }
+                res.add(map);
             }
-            res.add(map);
+        } catch (Exception e) {
+            return Result.fail(40402, e.getMessage());
         }
-        return Result.success("查找节点", res);
+        return Result.success(20402,"查找节点", res);
     }
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     @ApiOperation("查找节点内容")
     @PostMapping("/findDetail")
     public Result findNodeByID(@RequestParam(value = "id") @ApiParam(value = "id", required = true) String id) {
         System.out.println(id);
         List<Map<String, Object>> res = new ArrayList<>();
-        Object node = kgServer.findNodeByID(Long.parseLong(id));
-        NodeModel n = (NodeModel) node;
-        Map<String, Object> m = new HashMap<>();
-        m.put("property", "id");
-        m.put("content", n.getId());
-        res.add(m);
-        for (int i = 0; i < n.getPropertyList().size(); i++) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("property", n.getPropertyList().get(i).getKey());
-            map.put("content", n.getPropertyList().get(i).getValue());
-            res.add(map);
+        Object node;
+        try{
+            node = kgServer.findNodeByID(Long.parseLong(id));
+            NodeModel n = (NodeModel) node;
+            Map<String, Object> m = new HashMap<>();
+            m.put("property", "id");
+            m.put("content", n.getId());
+            res.add(m);
+            for (int i = 0; i < n.getPropertyList().size(); i++) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("property", n.getPropertyList().get(i).getKey());
+                map.put("content", n.getPropertyList().get(i).getValue());
+                res.add(map);
+            }
+        } catch (Exception e) {
+            return Result.fail(40403, e.getMessage());
         }
-        return Result.success("查找节点内容", res);
+
+        return Result.success(20403,"查找节点内容", res);
     }
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     @ApiOperation("查找邻居节点")
     @PostMapping("/findNerborByID")
     public Result findNeighbors(@RequestParam("id")
                                 @ApiParam(value = "id", required = true) String id) {
         System.out.println(id);
-        List<Map<String, Object>> res = kgServer.getNeighbors(Long.parseLong(id));
-        return Result.success("查询邻居节点", res);
+        List<Map<String, Object>> res;
+        try{
+            res = kgServer.getNeighbors(Long.parseLong(id));
+        } catch (Exception e) {
+            return Result.fail(40404, e.getMessage());
+        }
+        return Result.success(20404,"查询邻居节点", res);
     }
 
-    @ApiOperation("可视化")
-    @PostMapping("/paint")
-    public Result paint(@RequestParam("id")
-                        @ApiParam(value = "id", required = true) String id) {
-//        List neibor = kgServer.findNeiborByName(name);
-        GraphDTO neibor = kgServer.paint(Long.parseLong(id));
-        // System.out.println( ((Node) (neibor.getNodes().get(0))).get("properties").get("name"));
-        // nodes.get(0).get("properties").get("name");
-        return Result.success("查询邻居节点", neibor);
-    }
-
+    /**
+     *
+     * @param propertyList
+     * @return
+     */
     @ApiOperation("修改实体属性")
     @PostMapping("/modifyNode")
     public Result modifyNode(@RequestBody @ApiParam(value = "json字符串", required = true) JSONObject propertyList) {
         Integer id = (Integer) propertyList.get("id");
         System.out.println(id);
         propertyList.remove("id");
-        Object modify = kgServer.modifyNode(id.longValue(), propertyList);
-        return Result.success("修改实体成功", modify);
+        Object modify;
+        try{
+            modify = kgServer.modifyNode(id.longValue(), propertyList);
+        } catch (Exception e) {
+            return Result.fail(40405, e.getMessage());
+        }
+
+        return Result.success(20405,"修改实体成功", modify);
     }
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     @ApiOperation("删除实体")
     @PostMapping("/deleteNode")
     public Result deleteNode(@RequestParam("id")
                              @ApiParam(value = "id", required = true) String id) {
-        kgServer.deleteNode(Long.parseLong(id));
-        return Result.success("删除成功", null);
+        try {
+            kgServer.deleteNode(Long.parseLong(id));
+        } catch (Exception e) {
+            return Result.fail(40406, e.getMessage());
+        }
+        return Result.success(20406,"删除成功", null);
     }
 
-
+    /**
+     *
+     * @param id1
+     * @param id2
+     * @param type
+     * @return
+     */
     @ApiOperation("添加关系")
     @PostMapping("/addRelation")
     public Result addRelation(@RequestParam("id1") @ApiParam(value = "id1", required = true) String id1,
                               @RequestParam("id2") @ApiParam(value = "id2", required = true) String id2,
                               @RequestParam("type") @ApiParam(value = "关系类型", required = true) String type) {
-        String r = kgServer.addRelation(Long.parseLong(id1), Long.parseLong(id2), type);
-        return Result.success("添加关系", r);
+        String r;
+        try {
+            r = kgServer.addRelation(Long.parseLong(id1), Long.parseLong(id2), type);
+        } catch (Exception e) {
+            return Result.fail(40407, e.getMessage());
+        }
+        return Result.success(20407,"添加关系", r);
     }
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     @ApiOperation("删除关系")
     @PostMapping("/deleteRelation")
     public Result deleteRelation(@RequestParam("id")
                                  @ApiParam(value = "id", required = true) String id) {
-        kgServer.deleteRelation(Long.parseLong(id));
-        return Result.success("删除成功", null);
-    }
-
-    @PostMapping("/upload")
-    @ApiOperation("上传并保存文件")
-    public Result upload(@RequestParam("file")
-                             @ApiParam(value = "上传的文件", required = true) MultipartFile file) {
-        if (file == null || file.isEmpty()) {
-            return Result.fail(500, "文件有误");
+        try {
+            kgServer.deleteRelation(Long.parseLong(id));
+        } catch (Exception e) {
+            return Result.fail(40408, e.getMessage());
         }
-
-        String url = kgServer.upload(file);
-        return Result.success("文件上传成功", url);
+        return Result.success(20408,"删除成功", null);
     }
 
+//    @PostMapping("/upload")
+//    @ApiOperation("上传并保存文件")
+//    public Result upload(@RequestParam("file")
+//                             @ApiParam(value = "上传的文件", required = true) MultipartFile file) {
+//        if (file == null || file.isEmpty()) {
+//            return Result.fail(500, "文件有误");
+//        }
+//
+//        String url = kgServer.upload(file);
+//        return Result.success("文件上传成功", url);
+//    }
+
+    /**
+     *
+     * @param file
+     * @param type：entity为批量上传实体，relation为批量上传关系
+     * @return
+     */
     @PostMapping("/uploadTemplate")
     @ApiOperation("批量上传")
     public Result uploadExcel(@RequestParam("file") MultipartFile file, String type) {
@@ -173,12 +248,34 @@ public class KGController {
             return Result.fail(500, "文件有误");
         }
         System.out.println(type);
-        if (type.equals("entity")) {
-            kgServer.getEntFromFile(file);
-        } else {
-            kgServer.getRelFromFile(file);
+        try{
+            if (type.equals("entity")) {
+                kgServer.getEntFromFile(file);
+            } else {
+                kgServer.getRelFromFile(file);
+            }
+        } catch (Exception e) {
+            return Result.fail(40409, e.getMessage());
         }
-        return Result.success("批量上传成功", null);
+        return Result.success(20409,"批量上传成功", null);
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    @ApiOperation("可视化")
+    @PostMapping("/paint")
+    public Result paint(@RequestParam("id")
+                        @ApiParam(value = "id", required = true) String id) {
+        GraphDTO neibor;
+        try{
+            neibor = kgServer.paint(Long.parseLong(id));
+        } catch (Exception e) {
+            return Result.fail(40410, e.getMessage());
+        }
+        return Result.success(20410,"查询邻居节点", neibor);
     }
 
     @GetMapping("/getDBlist")
